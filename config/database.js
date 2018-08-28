22ca1238fe781e99c8bae1a4897c040c8dc15668
wkwksama <mailpakgasing@gmail.com>
2018-08-28 13:52:40 +0700

    ganti nama service choice jadi class service

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = {
   "development": {
     "username": process.env.PG_USERNAME || '',
     "password": process.env.PG_PASSWORD || '',
-    "database": process.env.PG_DATABASE || 'fls_choice',
+    "database": process.env.PG_DATABASE || 'fls_class',
     "host": process.env.PG_HOSTNAME || '',
     "dialect": "postgres",
     "logging": false,
